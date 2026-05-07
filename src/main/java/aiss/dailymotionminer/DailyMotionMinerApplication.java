d0cba5fce251b1436a607df2f015398ec38f4df4
Hugo Párraga <hugpardel@alum.us.es>
2026-05-07 18:18:33 +0200

    Packages created

--- src/main/java/aiss/dailymotionminer/DailyMotionMinerApplication.java
+++ src/main/java/aiss/dailymotionminer/DailyMotionMinerApplication.java
@@ -2,12 +2,18 @@ package aiss.dailymotionminer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DailyMotionMinerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DailyMotionMinerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DailyMotionMinerApplication.class, args);
+    }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
